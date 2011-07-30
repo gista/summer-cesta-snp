70a586bee73f953801d1d0dd73f54bbbaa53290a
Ivan Mincik <ivan.mincik@gmail.com>
2011-07-30 11:10:46 +0200

    * Merging with 'ivan-joomla-auth'.

--- djcode/auth.py
+++ djcode/auth.py
@@ -3,20 +3,28 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 
 class JoomlaAuth(object):
-	"""Authentication back-end class."""
-
+	"""Joomla authentication back-end class."""
+	
+	supports_object_permissions = False
+	supports_anonymous_user = True
+	supports_inactive_user = False
+	
 	def authenticate(self, jos_session_id=None):
 		try:
 			jos_session = Jos_session.objects.exclude(username='').get(session_id=jos_session_id)
 		except ObjectDoesNotExist:
 			return None
+		
 		try:
 			user = User.objects.get(username=jos_session.username)
 		except User.DoesNotExist:
 			user = User(username=jos_session.username)
 			user.set_unusable_password()
-			user.save()
 			#TODO: setting is_staff, is_superuser based on jos_user table
+			user.is_staff = False
+			user.is_superuser = False
+			user.save()
+		
 		return user
 
 	def get_user(self, user_id=None):
