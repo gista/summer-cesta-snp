ead4a14c0119eda5eb83af8cd9c73596f3fac7fc
Ivan Mincik <ivan.mincik@gmail.com>
2011-07-30 13:31:14 +0200

    * Adding 'testauth' page.

--- djcode/snppath/views.py
+++ djcode/snppath/views.py
@@ -1,4 +1,22 @@
+from django.conf import settings
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+
+def testauth(request):
+	resp = 'AUTHENTICATION TEST PAGE'
+	resp += '\nCOOKIES: %s' % str(request.COOKIES)
+	if request.COOKIES.has_key(settings.SNPPATH_COOKIE_SESSION_ID_NAME):
+		user = authenticate(jos_session_id=request.COOKIES[settings.SNPPATH_COOKIE_SESSION_ID_NAME])
+		resp += '\nUSER: %s' % user
+		
+		if user is not None and user.is_active:
+			login(request, user)
+		
+			resp += '\nIS_STAFF: %s' % user.is_staff
+			resp += '\nIS_SUPERUSER: %s' %  user.is_superuser
+	
+	return HttpResponse(resp, mimetype="text/plain")
 
 def home(request):
 	return render_to_response("index.html", {})
--- djcode/urls.py
+++ djcode/urls.py
@@ -5,5 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^$', 'djcode.snppath.views.home', name='home'),
+	url(r'^testauth$', 'djcode.snppath.views.testauth', name='testauth'),
 	url(r'^admin/', include(admin.site.urls)),
 )
