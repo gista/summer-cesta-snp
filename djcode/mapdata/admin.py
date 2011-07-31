8d1198050d153e03a09cc4826b1e1642d66549d9
Ivan Mincik <ivan.mincik@gmail.com>
2011-07-31 14:34:30 +0200

    * Improving 'mapdata' application admin page

--- djcode/mapdata/admin.py
+++ djcode/mapdata/admin.py
@@ -4,13 +4,25 @@ from models import Area, Poi, Path, Photo
 
 GMAP = GoogleMap()
 
-class GoogleAdmin(admin.OSMGeoAdmin):
+class Area_GoogleAdmin(admin.OSMGeoAdmin):
 	extra_js = [GMAP.api_url + GMAP.key]
 	map_template = 'gis/admin/google.html'
 	openlayers_url = 'http://openlayers.org/api/2.10/OpenLayers.js' #TODO: load OpenLayers from local sources
+	list_display = ('name', 'note',)
 
+class Path_GoogleAdmin(admin.OSMGeoAdmin):
+	extra_js = [GMAP.api_url + GMAP.key]
+	map_template = 'gis/admin/google.html'
+	openlayers_url = 'http://openlayers.org/api/2.10/OpenLayers.js' #TODO: load OpenLayers from local sources
+	list_display = ('area', 'type', 'note',)
+
+class Poi_GoogleAdmin(admin.OSMGeoAdmin):
+	extra_js = [GMAP.api_url + GMAP.key]
+	map_template = 'gis/admin/google.html'
+	openlayers_url = 'http://openlayers.org/api/2.10/OpenLayers.js' #TODO: load OpenLayers from local sources
+	list_display = ('name', 'active', 'area', 'type', 'priority', 'note',)
 
 admin.site.register(Photo)
-admin.site.register(Area, GoogleAdmin)
-admin.site.register(Poi, GoogleAdmin)
-admin.site.register(Path, GoogleAdmin)
+admin.site.register(Area, Area_GoogleAdmin)
+admin.site.register(Path, Path_GoogleAdmin)
+admin.site.register(Poi, Poi_GoogleAdmin)
