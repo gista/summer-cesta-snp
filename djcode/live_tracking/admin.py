2c8755e8c9074fdf76fc9e8e8a199ca8428483b8
Marcel Dancak <marcel.dancak@gista.sk>
2011-08-08 16:28:49 +0200

    * [MODEL] change models and admin classes in live_tracking app (issue #29)

--- djcode/live_tracking/admin.py
+++ djcode/live_tracking/admin.py
@@ -1,11 +1,14 @@
 from django.contrib.gis import admin
 from django.contrib.gis.maps.google import GoogleMap
 from django.conf import settings
-from models import User, Message, Sync_log
+from models import User, Track, Message, Sync_log
 
 class User_admin(admin.ModelAdmin):
-	list_display = ("id", "username", "first_name", "last_name", "track_name", "email", "phone", "description", "is_active",)
+	list_display = ("id", "username", "first_name", "last_name", "email", "phone",)
 	search_fields = ("username", "first_name", "last_name", "phone",)
+
+class Track_admin(admin.ModelAdmin):
+	list_display = ("name", "description")
 
 class Sync_log_admin(admin.ModelAdmin):
 	list_display = ("time", "success")
@@ -13,16 +16,17 @@ class Sync_log_admin(admin.ModelAdmin):
 
 GMAP = GoogleMap()
 
-class Message_GoogleAdmin(admin.OSMGeoAdmin):
-	extra_js = [GMAP.api_url + GMAP.key]
+class Message_GeoAdmin(admin.OSMGeoAdmin):
+	extra_js = ["http://maps.google.com/maps/api/js?v=3.2&sensor=false"]
 	map_template = 'gis/admin/geoadmin.html'
-	openlayers_url = '%sjs/openlayers/OpenLayers.js' % settings.STATIC_URL
-	list_display = ("user", "time", "text",)
-	list_filter = ("user", "time",)
-	search_fields = ("user",)
+	openlayers_url = '%sjs/openlayers-211-rc1/OpenLayers.js' % settings.STATIC_URL
+	list_display = ("user", "track", "time", "text",)
+	list_filter = ("user", "track", "time",)
+	search_fields = ("user", "track")
 	ordering = ("time",)
 
 
 admin.site.register(User, User_admin)
-admin.site.register(Message, Message_GoogleAdmin)
+admin.site.register(Track, Track_admin)
+admin.site.register(Message, Message_GeoAdmin)
 admin.site.register(Sync_log, Sync_log_admin)
--- djcode/live_tracking/models.py
+++ djcode/live_tracking/models.py
@@ -8,19 +8,24 @@ class User(models.Model):
 	last_name =		models.CharField(max_length=50, blank=True)
 	email =			models.EmailField(blank=True)
 	phone =			models.CharField(max_length=30, blank=True)
-	is_active = 		models.BooleanField(default=True, 
-					help_text='Designates whether this user is still active on path.')
-	track_name = 		models.CharField(max_length=50, help_text='The name of the track.')
-	description = 		models.TextField(blank=True, help_text='Description of the track.')
-	date_from = 		models.DateField(blank=True, null=True, help_text='Date, when track began.')
-	date_to = 		models.DateField(blank=True, null=True, help_text='Date, when track ended.')
 
 	def __unicode__(self):
 		return "%s (%s)" % (self.username, self.track_name)
 
+class Track(models.Model):
+	""" Class representing a track in live tracking application."""
+	name = 			models.CharField(max_length=50, help_text='The name of the track.')
+	description = 	models.TextField(blank=True, help_text='Description of the track.')
+	users = 		models.ManyToManyField(User, blank=True, help_text='Inactive users assigned to the track.', related_name="live_tracking_track_set")
+	active_users = 	models.ManyToManyField(User, blank=True, help_text='Active users assigned to the track.')
+
+	def __unicode__(self):
+		return "%s (%s)" % (self.name, self.description)
+
 class Message(models.Model):
 	""" Class representing a message sent by a user."""
 	user = 		models.ForeignKey(User, help_text='User, who sent a message.')
+	track = 	models.ForeignKey(Track, help_text='Track of the user.')
 	time = 		models.DateTimeField(help_text='Time of sending the message.')
 	text = 		models.TextField(blank=True, help_text='Text of the message.')
 	
