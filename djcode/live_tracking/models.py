4f6c9f9b3ce1a4406f8a7bc6184490179a29e0f1
Matus <matusvalo@gmail.com>
2011-08-11 23:45:53 +0200

    * Added verbose_name to model fields and translation. Fix #30, #31

--- djcode/live_tracking/models.py
+++ djcode/live_tracking/models.py
@@ -1,34 +1,35 @@
 from django.contrib.gis.db import models
+from django.utils.translation import ugettext_lazy as _
 
 class User(models.Model):
 	""" Class representing a user in live tracking application."""
-	id = 			models.IntegerField(primary_key=True)
-	username = 		models.CharField(max_length=50)
-	first_name =		models.CharField(max_length=50, blank=True)
-	last_name =		models.CharField(max_length=50, blank=True)
-	email =			models.EmailField(blank=True)
-	phone =			models.CharField(max_length=30, blank=True)
+	id = 			models.IntegerField(_(u'id'), primary_key=True, help_text=_(u'ID of the user'))
+	username = 		models.CharField(_(u'username'), max_length=50, help_text=_(u'Username of the user'))
+	first_name =		models.CharField(_(u'first name'), max_length=50, blank=True, help_text=_(u'First name of the user'))
+	last_name =		models.CharField(_(u'last name'), max_length=50, blank=True, help_text=_(u'Last name of the user'))
+	email =			models.EmailField(_(u'email'), blank=True, help_text=_(u'Email of the user'))
+	phone =			models.CharField(_(u'phone number'), max_length=30, blank=True, help_text=_(u'Phone number of the user'))
 
 	def __unicode__(self):
 		return "%s" % self.username
 
 class Track(models.Model):
 	""" Class representing a track in live tracking application."""
-	name = 			models.CharField(max_length=50, help_text='The name of the track.')
-	description = 	models.TextField(blank=True, help_text='Description of the track.')
-	user = 			models.ForeignKey(User, blank=True, help_text='User assigned to the track.')#related_name="live_tracking_track_set"
-	is_active = 	models.BooleanField(blank=True, help_text='Designates whether user is still active on track.')
+	name = 		models.CharField(_(u'name'), max_length=50, help_text=_(u'The name of the track.'))
+	description = 	models.TextField(_(u'description'), blank=True, help_text=_(u'Description of the track.'))
+	user = 		models.ForeignKey(User, blank=True, verbose_name=_(u'user'), help_text=_(u'User assigned to the track.'))#related_name="live_tracking_track_set"
+	is_active = 	models.BooleanField(_(u'active'), blank=True, help_text=_(u'Designates whether user is still active on track.'))
 
 	def __unicode__(self):
 		return "%s" % self.name
 
 class Message(models.Model):
 	""" Class representing a message sent by a user."""
-	track = 	models.ForeignKey(Track, help_text='Track of the user.')
-	time = 		models.DateTimeField(help_text='Time of sending the message.')
-	text = 		models.TextField(blank=True, help_text='Text of the message.')
-	
-	the_geom = 	models.PointField(null=True, help_text='Spatial point of source of the message.')
+	track = 	models.ForeignKey(Track, verbose_name=_(u'track'), help_text=_(u'Track of the user.'))
+	time = 		models.DateTimeField(_(u'time'), help_text=_(u'Time of sending the message.'))
+	text = 		models.TextField(_(u'text'), blank=True, help_text=_(u'Text of the message.'))
+
+	the_geom = 	models.PointField(_(u'the geom'), null=True, help_text=_(u'Spatial point of source of the message.'))
 	objects = 	models.GeoManager()
 
 	def user(self):
@@ -44,13 +45,13 @@ class Message(models.Model):
 
 class Sync_log(models.Model):
 	"""Log of synchronizing DB from Freemap API."""
-	time = 		models.DateTimeField(help_text='Time of synchronizing.')
-	success = 	models.BooleanField(help_text='Success of synchronizing.')
-	
+	time = 		models.DateTimeField(_(u'time'), help_text=_(u'Time of synchronizing.'))
+	success = 	models.BooleanField(_(u'success'), help_text=_(u'Success of synchronizing.'))
+
 	def __unicode__(self):
 		return "%s" % self.time
 
 	class Meta:
 		get_latest_by =	'time'
 		ordering = ['time']
-		verbose_name = u'sync log'
+		verbose_name = _(u'sync log')
--- djcode/mapdata/models.py
+++ djcode/mapdata/models.py
@@ -1,12 +1,13 @@
 from django.contrib.gis.db import models
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 class Area(models.Model):
 	"""Model representing area, which contains path and Points of interest."""
-	name = models.CharField(max_length = 50, help_text='Name of the area.')
-	note = models.TextField(blank = True, help_text='Note about the area.')
-	
-	the_geom = models.PolygonField()
+	name = models.CharField(_(u'name'), max_length = 50, help_text=_(u'Name of the area.'))
+	note = models.TextField(_(u'note'), blank = True, help_text=_(u'Note about the area.'))
+
+	the_geom = models.PolygonField(_(u'the geom'), help_text=_(u'Spatial representation of the area in the WGS84.'))
 	objects  = models.GeoManager()
 
 	def __unicode__(self):
@@ -14,11 +15,11 @@ class Area(models.Model):
 
 class Path(models.Model):
 	"""Model representing path of Cesta hrdinov SNP and other surrounding paths."""
-	area    = models.ForeignKey(Area, help_text='Area, which path belongs to.')
-	type    = models.IntegerField(choices=settings.SNP_PATH_TYPES, default=1, help_text='Type of the path (default 1).')
-	note    = models.TextField(blank = True, help_text = 'note about the path')
+	area    = models.ForeignKey(Area, verbose_name=_(u'area'), help_text=_(u'Area, which path belongs to.'))
+	type    = models.IntegerField(_(u'type'), choices=settings.SNP_PATH_TYPES, default=1, help_text=_(u'Type of the path (default 1).'))
+	note    = models.TextField(_(u'note'), blank = True, help_text = _(u'note about the path'))
 
-	the_geom = models.LineStringField(help_text = 'Spatial representation of the path in the WGS84.')
+	the_geom = models.LineStringField(_(u'the geom'), help_text = _(u'Spatial representation of the path in the WGS84.'))
 	objects  = models.GeoManager()
 
 	def __unicode__(self):
@@ -26,37 +27,37 @@ class Path(models.Model):
 
 class Jos_article_id(models.Model):
 	"""Model representing ID of article in DB of Joomla"""
-	id = models.IntegerField(primary_key = True)
+	id = models.IntegerField(_(u'id'), primary_key = True)
 
 class Jos_photo_id(models.Model):
 	"""Model representing ID of photo in DB of Joomla"""
-	id = models.IntegerField(primary_key = True)
+	id = models.IntegerField(_(u'id'), primary_key = True)
 
 class Photo(models.Model):
-	title = models.CharField(max_length=50)
-	desctiption = models.CharField(max_length=100, blank=True)
-	photo = models.ImageField(upload_to='/tmp/photos/%Y/%m/%d', blank=True)
-	
+	title = models.CharField(_(u'title'), max_length=50)
+	desctiption = models.CharField(_(u'description'), max_length=100, blank=True)
+	photo = models.ImageField(_(u'photo'), upload_to='/tmp/photos/%Y/%m/%d', blank=True)
+
 	def __unicode__(self):
 		return self.title
 
 class Poi(models.Model):
 	"""Model representing Point of interest (POI)."""
-	name           = models.CharField(max_length=50, help_text='Name of the POI.')
-	area           = models.ForeignKey(Area, help_text='Area, which POI is situated in.')
-	type           = models.IntegerField(choices=settings.SNP_POI_TYPES, help_text='Type of the POI.')
-	active         = models.BooleanField(default=True, help_text='Boolean of controlling displaying POI in the map.')
-	priority       = models.IntegerField(default=5, help_text='Displaying priority of the POI in the map.')
-	note           = models.TextField(blank=True, help_text='Note about the POI.')
-	photo          = models.ManyToManyField(Photo, blank=True, null=True, help_text='Photo of the POI.')
-	jos_article_id = models.ManyToManyField(Jos_article_id, blank=True, null=True,
-						help_text='Many-to-many relation with article ids (from Joomla DB).')
-	jos_photo_id   = models.ManyToManyField(Jos_photo_id, blank=True, null=True,
-						help_text='Many-to-many relation with photo ids (from Joomla DB).')
-	created_by     = models.CharField(max_length=50, blank=True, help_text='Name of the creator of the POI.')
-	created_at     = models.DateTimeField(blank=True, null=True, help_text='Date-time stamp of creation of the POI.')
+	name           = models.CharField(_(u'name'), max_length=50, help_text=_(u'Name of the POI.'))
+	area           = models.ForeignKey(Area, help_text=_(u'Area, which POI is situated in.'), verbose_name=_(u'area'))
+	type           = models.IntegerField(_(u'type'), choices=settings.SNP_POI_TYPES, help_text=_(u'Type of the POI.'))
+	active         = models.BooleanField(_(u'active'), default=True, help_text=_(u'Boolean of controlling displaying POI in the map.'))
+	priority       = models.IntegerField(_(u'priority'), default=5, help_text=_(u'Displaying priority of the POI in the map.'))
+	note           = models.TextField(_(u'note'), blank=True, help_text=_(u'Note about the POI.'))
+	photo          = models.ManyToManyField(Photo, blank=True, null=True, verbose_name=_(u'list of photos'), help_text=_(u'Photo of the POI.'))
+	jos_article_id = models.ManyToManyField(Jos_article_id, blank=True, null=True, verbose_name=_(u'list of articles'),
+						help_text=_(u'Many-to-many relation with article ids (from Joomla DB).'))
+	jos_photo_id   = models.ManyToManyField(Jos_photo_id, blank=True, null=True, verbose_name=_(u'list of joomla photos'),
+						help_text=_(u'Many-to-many relation with photo ids (from Joomla DB).'))
+	created_by     = models.CharField(_(u'author'), max_length=50, blank=True, help_text=_(u'Name of the creator of the POI.'))
+	created_at     = models.DateTimeField(_(u'creation time'), blank=True, null=True, help_text=_(u'Date-time stamp of creation of the POI.'))
 
-	the_geom       = models.PointField(help_text='Spatial representation of the POI in the WGS84.')
+	the_geom       = models.PointField(_(u'the geom'), help_text=_(u'Spatial representation of the POI in the WGS84.'))
 	objects        = models.GeoManager()
 
 	@property
@@ -75,8 +76,8 @@ class Poi(models.Model):
 
 	class Meta:
 		get_latest_by = 'created_at'
-		verbose_name = 'Point of interest'
-		verbose_name_plural = 'Points of interest'
+		verbose_name = _(u'Point of interest')
+		verbose_name_plural = _(u'Points of interest')
 
 	def __unicode__(self):
 		return self.name
