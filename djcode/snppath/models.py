6f3597b6258900da559555ff07bfcae050ec1d72
Matus <matusvalo@gmail.com>
2011-08-24 23:03:20 +0200

    * Added model for help.

--- djcode/snppath/models.py
+++ djcode/snppath/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+class Help(models.Model):
+	text = models.TextField(_(u'help text'), help_text=_(u'Help text in HTML format.'))
+	language = models.CharField(_(u'language code'), max_length = 10, help_text=_(u'Language code of the text language.'))
