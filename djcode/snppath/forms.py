9fba659740efd3101e7d69c1850442afe0f617ea
Matus <matusvalo@gmail.com>
2011-08-29 23:35:13 +0200

    * Added support of advertisment in admin pages. fix #76

---
+++ djcode/snppath/forms.py
@@ -0,0 +1,76 @@
+from django import forms
+from snppath.models import *
+from django.core.validators import URLValidator, MaxLengthValidator
+from validators import RangeValidator
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+from django.core.files.images import get_image_dimensions
+
+class TopAdvertismentAdminForm(forms.ModelForm):
+	class Meta:
+		model = Top_advertisment
+
+	def clean_transparency(self):
+		transparency = self.cleaned_data['transparency']
+		RangeValidator(0, 1)(transparency)
+		return transparency
+
+	def clean_image(self):
+		image = self.cleaned_data['image']
+		width, height = get_image_dimensions(image)
+		if width != TOP_AD_IMG_WIDTH or height != TOP_AD_IMG_HEIGHT:
+			raise ValidationError(_(u'Image must have {0}x{1} px size.'.format(TOP_AD_IMG_WIDTH,
+											 TOP_AD_IMG_HEIGHT)))
+		return image
+
+	def clean_title(self):
+		title = self.cleaned_data['title']
+		try:
+			MaxLengthValidator(MAX_AD_TITLE_LEN)(title)
+		except ValidationError:
+			raise ValidationError(_(u'Must have at most {0} characters'.format(MAX_AD_TITLE_LEN)))
+		return title
+
+	def clean_url(self):
+		url = self.cleaned_data['url']
+		try:
+			MaxLengthValidator(MAX_AD_URL_LEN)(url)
+		except ValidationError:
+			raise ValidationError(_(u'Must have at most {0} characters.'.format(MAX_AD_URL_LEN)))
+		try:
+			URLValidator()(url)
+		except ValidationError:
+			raise ValidationError(_(u'Invalid URL.'))
+		return url
+
+class SideAdvertismentAdminForm(forms.ModelForm):
+	class Meta:
+		model = Side_advertisment
+
+	def clean_title(self):
+		title = self.cleaned_data['title']
+		try:
+			MaxLengthValidator(MAX_AD_TITLE_LEN)(title)
+		except ValidationError:
+			raise ValidationError(_(u'Must have at most {0} characters'.format(MAX_AD_TITLE_LEN)))
+		return title
+
+	def clean_image(self):
+		image = self.cleaned_data['image']
+		width, height = get_image_dimensions(image)
+		if width != SIDE_AD_IMG_WIDTH or height != SIDE_AD_IMG_HEIGHT:
+			raise ValidationError(_(u'Image must have {0}x{1} px size.'.format(SIDE_AD_IMG_WIDTH,
+											 SIDE_AD_IMG_HEIGHT)))
+		return image
+
+	def clean_url(self):
+		url = self.cleaned_data['url']
+		try:
+			MaxLengthValidator(MAX_AD_URL_LEN)(url)
+		except ValidationError:
+			raise ValidationError(_(u'Must have at most {0} characters.'.format(MAX_AD_URL_LEN)))
+		try:
+			URLValidator()(url)
+		except ValidationError:
+			raise ValidationError(_(u'Invalid URL.'))
+		return url
--- djcode/validators.py
+++ djcode/validators.py
@@ -2,9 +2,18 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 def validate_lat(value):
-	if(value < -90 or value > 90):
+	if value < -90 or value > 90:
 		raise ValidationError(_(u'Exceeded latitude!'))
 
 def validate_lon(value):
-	if(value < -180 or value > 180):
+	if value < -180 or value > 180:
 		raise ValidationError(_(u'Exceeded longitude!'))
+
+class RangeValidator(object):
+	def __init__(self, min_value, max_value):
+		self.vmax = max_value
+		self.vmin = min_value
+
+	def __call__(self, value):
+		if not self.vmin <= value <= self.vmax:
+			raise ValidationError(_(u'Value must be between {0} and {1}.'.format(self.vmin, self.vmax)))
