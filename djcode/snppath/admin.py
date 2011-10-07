8fc423c4b76e048c01646212d4b1a57fd6eb80fc
Ivan Mincik <ivan.mincik@gista.sk>
2011-10-07 22:08:57 +0100

    * Adding Help to Admin page

--- djcode/snppath/admin.py
+++ djcode/snppath/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from django.contrib.sites.models import Site
+from models import Help
+
+class Help_admin(admin.ModelAdmin):
+	list_display = ("language",)
+	ordering = ("language",)
 
 admin.site.unregister(Site)
+admin.site.register(Help, Help_admin)
