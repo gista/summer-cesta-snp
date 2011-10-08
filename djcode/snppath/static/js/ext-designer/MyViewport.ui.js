24d650282b7ac43135faddd46b1f7db61482b276
Peter Margetiak <peter.margetiak@gmail.com>
2011-10-08 16:09:08 +0100

    * fix #123 Date in 'Path ended' aligned to right.

--- djcode/snppath/static/js/ext-designer/MyViewport.ui.js
+++ djcode/snppath/static/js/ext-designer/MyViewport.ui.js
@@ -150,7 +150,8 @@ MyViewportUi = Ext.extend(Ext.Viewport, {
                                                 dataIndex: 'last_location_time',
                                                 sortable: true,
 						id: 'last_location_time',
-                                                format: 'd.m.Y'
+                                                format: 'd.m.Y',
+						align: 'right'
                                             }
                                         ]
                                     },
