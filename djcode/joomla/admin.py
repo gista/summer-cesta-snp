b102641447d6e3bfc0292ca90f0dcdb7a6ad62e5
Ivan Mincik <ivan.mincik@gista.sk>
2011-10-17 18:10:17 +0100

    * Removing Admin 'actions' from Joomla's models

--- djcode/joomla/admin.py
+++ djcode/joomla/admin.py
@@ -6,6 +6,7 @@ class Jos_session_admin(admin.ModelAdmin):
 	readonly_fields = Jos_session._meta.get_all_field_names()
 	search_fields = ("username","session_id",)
 	ordering = ("time",)
+	actions = None
 
 class Jos_joom_gallery_catg_admin(admin.ModelAdmin):
 	list_display = ("cid", "name",)
@@ -13,21 +14,25 @@ class Jos_joom_gallery_catg_admin(admin.ModelAdmin):
 		"published", "owner", "catimage", "img_position", "catpath",)
 
 	search_fields = ("name",)
+	actions = None
 
 class Jos_joom_gallery_admin(admin.ModelAdmin):
 	list_display = ("id", "catid", "imgtitle", "imgauthor", "imgtext",)
 	readonly_fields = Jos_joom_gallery._meta.get_all_field_names()
 	search_fields = ("imgtitle",)
+	actions = None
 
 class Jos_content_admin(admin.ModelAdmin):
 	list_display = ("id", "title", "alias", "title_alias", "introtext",)
 	readonly_fields = Jos_content._meta.get_all_field_names()
 	search_fields = ("title",)
+	actions = None
 
 class Jos_user_admin(admin.ModelAdmin):
 	list_display = ("id", "username", "name", "email", "usertype",)
 	readonly_fields = Jos_user._meta.get_all_field_names()
 	search_fields = ("username", "name", "email",)
+	actions = None
 
 admin.site.register(Jos_session, Jos_session_admin)
 admin.site.register(Jos_joom_gallery, Jos_joom_gallery_admin)
